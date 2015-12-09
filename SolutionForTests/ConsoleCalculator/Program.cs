9c3e139186bf374ab9414d5843248591c74cc02f
mjuskiewicz <mikolaj.juskiewicz@gmail.com>
2015-12-09 23:56:09 +0100

    - Simple commit

---
+++ SolutionForTests/ConsoleCalculator/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Calculator().Start();
+        }
+    }
+
+    public class Calculator
+    {
+        public void Start()
+        {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
