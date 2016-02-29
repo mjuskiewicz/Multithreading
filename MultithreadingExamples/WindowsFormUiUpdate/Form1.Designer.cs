487db8300d0d05ac8b73df507be6627e1839a18b
mjuskiewicz <mikolaj.juskiewicz@gmail.com>
2016-02-29 21:06:15 +0100

    Windows Forms Application that contains a example of updating the UI from a thread other than the UI thread.

---
+++ MultithreadingExamples/WindowsFormUiUpdate/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormUiUpdate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonRed = new System.Windows.Forms.Button();
+            this.buttonBlue = new System.Windows.Forms.Button();
+            this.pictureBoxBlue = new System.Windows.Forms.PictureBox();
+            this.pictureBoxRed = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonRed
+            // 
+            this.buttonRed.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonRed.Location = new System.Drawing.Point(0, 238);
+            this.buttonRed.Name = "buttonRed";
+            this.buttonRed.Size = new System.Drawing.Size(361, 23);
+            this.buttonRed.TabIndex = 0;
+            this.buttonRed.Text = "Red square - Start - SynchronizationContext";
+            this.buttonRed.UseVisualStyleBackColor = true;
+            this.buttonRed.Click += new System.EventHandler(this.buttonRed_Click);
+            // 
+            // buttonBlue
+            // 
+            this.buttonBlue.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.buttonBlue.Location = new System.Drawing.Point(0, 215);
+            this.buttonBlue.Name = "buttonBlue";
+            this.buttonBlue.Size = new System.Drawing.Size(361, 23);
+            this.buttonBlue.TabIndex = 1;
+            this.buttonBlue.Text = "Blue square - Start - Control Invoke";
+            this.buttonBlue.UseVisualStyleBackColor = true;
+            this.buttonBlue.Click += new System.EventHandler(this.buttonBlue_Click);
+            // 
+            // pictureBoxBlue
+            // 
+            this.pictureBoxBlue.BackColor = System.Drawing.Color.Blue;
+            this.pictureBoxBlue.Location = new System.Drawing.Point(0, 0);
+            this.pictureBoxBlue.Name = "pictureBoxBlue";
+            this.pictureBoxBlue.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxBlue.TabIndex = 2;
+            this.pictureBoxBlue.TabStop = false;
+            // 
+            // pictureBoxRed
+            // 
+            this.pictureBoxRed.BackColor = System.Drawing.Color.Red;
+            this.pictureBoxRed.Location = new System.Drawing.Point(0, 50);
+            this.pictureBoxRed.Name = "pictureBoxRed";
+            this.pictureBoxRed.Size = new System.Drawing.Size(50, 50);
+            this.pictureBoxRed.TabIndex = 3;
+            this.pictureBoxRed.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(361, 261);
+            this.Controls.Add(this.pictureBoxRed);
+            this.Controls.Add(this.pictureBoxBlue);
+            this.Controls.Add(this.buttonBlue);
+            this.Controls.Add(this.buttonRed);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxBlue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxRed)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonRed;
+        private System.Windows.Forms.Button buttonBlue;
+        private System.Windows.Forms.PictureBox pictureBoxBlue;
+        private System.Windows.Forms.PictureBox pictureBoxRed;
+    }
+}
+
---
+++ MultithreadingExamples/WindowsFormUiUpdate/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormUiUpdate
+{
+    public partial class Form1 : Form
+    {
+        private SynchronizationContext uiContext;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            uiContext = WindowsFormsSynchronizationContext.Current;
+        }
+
+        private void SimpleInvokeExample()
+        {
+            for (int i = 0; i < Width; i += 10)
+            {
+                this.Invoke(new MethodInvoker(() =>
+                {
+                    pictureBoxRed.Left = i;
+                }));
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void SynchronizationContextExample()
+        {
+            for (int i = 0; i < Width; i += 10)
+            {
+                uiContext.Post(new SendOrPostCallback((parameter) =>
+                {
+                    pictureBoxBlue.Left = (int)parameter;
+                }), i);
+
+                Thread.Sleep(100);
+            }
+        }
+
+        private void buttonBlue_Click(object sender, EventArgs e)
+        {
+            var blueThread = new Thread(SynchronizationContextExample);
+            blueThread.Start();
+        }
+
+        private void buttonRed_Click(object sender, EventArgs e)
+        {
+            var redThread = new Thread(SimpleInvokeExample);
+            redThread.Start();
+        }
+    }
+}
