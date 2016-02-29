66be2e3a6beba7ae39a02b1f37dd330ae75fdba7
mjuskiewicz <mikolaj.juskiewicz@gmail.com>
2016-02-29 21:09:57 +0100

    The console applicaion includes an example of using ManualResetEvent.

---
+++ MultithreadingExamples/ManualResetEventSlimExample/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading;
+
+namespace ManualResetEventSlimExample
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            ManualResetEvent threadOneController = new ManualResetEvent(true);
+            ManualResetEvent threadTwoController = new ManualResetEvent(false);
+
+            ThreadStart action1 = new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    threadOneController.WaitOne();
+                    threadTwoController.Reset();
+                    Console.WriteLine("{0} --- Thread 1 is doing some actions", DateTime.Now);
+                    threadTwoController.Set();
+                }
+            });
+
+            ThreadStart action2 = new ThreadStart(() =>
+            {
+                while (true)
+                {
+                    threadTwoController.WaitOne();
+                    threadOneController.Reset();
+                    Console.WriteLine("{0} --- Thread 2 is doing some actions", DateTime.Now);
+                    threadOneController.Set();
+                }
+            });
+
+            var t1 = new Thread(action1);
+            var t2 = new Thread(action2);
+
+            t1.Start();
+            t2.Start();
+        }
+    }
+}
