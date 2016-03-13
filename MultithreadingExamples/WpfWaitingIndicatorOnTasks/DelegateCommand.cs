bb55eb5b55521e9199b8ed95fe0dc5a49a2f4145
mjuskiewicz <mikolaj.juskiewicz@gmail.com>
2016-03-13 20:04:21 +0100

    Added missing file.

---
+++ MultithreadingExamples/WpfWaitingIndicatorOnTasks/DelegateCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfWaitingIndicatorOnTasks
+{
+    public class DelegateCommand : ICommand
+    {
+        private Action _action;
+        private Func<bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute != null && _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+        
+        public DelegateCommand(Action action,  Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+    }
+}
