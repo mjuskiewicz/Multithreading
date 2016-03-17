316951772d249f9bdecce9fde51e2c0f346008f4
mjuskiewicz <mikolaj.juskiewicz@gmail.com>
2016-03-17 23:00:01 +0100

    The simple WPF application. The app allows users to send a query to the Bing engine for selected term. The program uses Reactive Extensions.

---
+++ MultithreadingExamples/WpfBinqSearchWithRx/Commons/NotificationObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+
+namespace WpfBingSearchWithRx.Commons
+{
+    public class NotificationObject : INotifyPropertyChanged
+    {
+        public IConnectableObservable<string> PropertyChangedStream { get; private set; }
+        private ReplaySubject<string> streamSource = new ReplaySubject<string>();
+
+        public NotificationObject()
+        {
+            PropertyChangedStream = streamSource.AsObservable().Publish();
+            PropertyChangedStream.Connect();
+        }
+
+        protected string GetPropertyName<TProperty>(Expression<Func<TProperty>> projection)
+        {
+            var memberExpression = (MemberExpression)projection.Body;
+            return memberExpression.Member.Name;
+        }
+
+        protected void RaisePropertyChanged<TProperty>(Expression<Func<TProperty>> projection)
+        {
+            var propertyName = GetPropertyName(projection);
+            RaisePropertyChanged(propertyName);
+        }
+
+        private void RaisePropertyChanged(string name)
+        {
+            streamSource.OnNext(name);
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ MultithreadingExamples/WpfBinqSearchWithRx/ViewModels/ISearchViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Bing;
+using System.Collections.Generic;
+
+namespace WpfBingSearchWithRx.ViewModels
+{
+    public interface ISearchViewModel
+    {
+        string SearchTerm { get; set; }
+        string Key { get; }
+        IEnumerable<WebResult> ListOfPages { get; }
+    }
+}
